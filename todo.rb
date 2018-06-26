b89d1c92f5e59755ec382f89e5f0dd5b47b470b4
matochondrion <matochondrion@gmail.com>
2018-06-26 05:45:17 +0000

    add list edit functionality

--- todo.rb
+++ todo.rb
@@ -52,8 +52,32 @@ post '/lists' do
   end
 end
 
-get "/lists/:id" do
+get '/lists/:id' do
   id = params[:id].to_i
   @list = session[:lists][id]
   erb :list, layout: :layout
+end
+
+# Edit an existing todo list
+get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# Update an existing todo list
+post '/lists/:id' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+  redirect "/lists/#{id}"
+  end
 end
